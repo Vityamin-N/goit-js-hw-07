a9c9c8ee91d7e892d4a698e85caba97b4c62663d
Vityamin-N <heartless41196@gmail.com>
2022-02-02 21:39:51 +0200

    adding more images

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -62,4 +62,130 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/25/16/01/sky-6966721__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/25/16/01/sky-6966721_1280.jpg',
+    description: 'Sky and clouds',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2021/11/13/23/06/tree-6792528_1280.jpg',
+    description: 'Tree in the park',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/18/16/49/town-6947538__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/18/16/49/town-6947538_1280.jpg',
+    description: 'Winter evening town',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/23/13/04/sparrow-6960807__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/23/13/04/sparrow-6960807_1280.jpg',
+    description: 'A little sparrow',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/21/17/04/bird-6955201__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/21/17/04/bird-6955201_1280.jpg',
+    description: 'Color parrot',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/19/00/36/sea-6948569__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/19/00/36/sea-6948569_1280.jpg',
+    description: 'Blue sea and mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/16/10/51/leaves-6941709__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/16/10/51/leaves-6941709_1280.jpg',
+    description: 'Green leaves',
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/07/08/09/02/star-wars-4324301__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/07/08/09/02/star-wars-4324301_1280.jpg",
+    description: "TIE fighter in forest",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/12/19/28/mountains-6933693__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/12/19/28/mountains-6933693_1280.jpg',
+    description: 'House near mountains',
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    description: "Stars and Galaxy",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/05/01/21/20/earth-1365995__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/05/01/21/20/earth-1365995_1280.jpg",
+    description: "Earth and Moon",
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2022/01/08/14/09/mont-saint-michel-6924072__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2022/01/08/14/09/mont-saint-michel-6924072_1280.jpg',
+    description: 'Castle near a sea',
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2021/10/09/07/06/natural-6693234__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2021/10/09/07/06/natural-6693234_1280.jpg",
+    description: "Dakezawa valley of Mt. Hodaka (3,190m)",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2020/07/09/10/32/water-5386820__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2020/07/09/10/32/water-5386820_1280.jpg",
+    description: "Castle near water",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2017/06/10/05/26/rice-terraces-2389023__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2017/06/10/05/26/rice-terraces-2389023_1280.jpg",
+    description: "Rice terraces",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2015/03/31/23/08/neuschwanstein-701732__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2015/03/31/23/08/neuschwanstein-701732_1280.jpg",
+    description: "Neuschwanstein castle",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2017/04/15/11/51/mt-fuji-2232246__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2017/04/15/11/51/mt-fuji-2232246_1280.jpg",
+    description: "Mount Fuji",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2015/05/29/17/39/noble-789501__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2015/05/29/17/39/noble-789501_1280.jpg",
+    description: "Hofburg castle",
+  },
 ];
